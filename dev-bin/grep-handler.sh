cbedbf6a4d2f329ad5db283987a7b96cf2b62c63
Aaron Michaux <aaron.michaux@broadcom.com>
2022-02-07 14:04:06 -0500

    removed broadcom stuff

--- dev-bin/grep-handler.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cat "$1" | grep -E 'handler.cc' | sed 's,^[^ ]* \([0-9:\.]*\).*flow_event_handler.cc:[0-9]*\],\1,'
-
-
-
--- local-bin/daily-backup.sh
+++
@@ -1,6 +0,0 @@
-
-source /Users/am894222/.bashrc
-
-NOW="$(date +%Y-%m-%d_%H-%M-%S)"
-
-cp $HOME/Development/SWG_NGP_kestrel/zz-run.sh $HOME/Documents/bak/zz-run.sh.$NOW
