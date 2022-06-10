0ed58c5fe01d06e61abaeff06bfe483cc33831fa
Aaron Michaux <aaron@pageofswords.net>
2022-06-10 08:39:31 -0400

    boost stuff

--- build-stuff/build-boost.sh
+++ build-stuff/build-boost.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-BOOST_VERSION=1_69_0
+BOOST_VERSION=1_78_0
 PREFIX=/usr/local
 NO_CLEANUP=1
+
+URL="https://boostorg.jfrog.io/artifactory/main/release/$(echo $BOOST_VERSION | sed 's,_,.,g')/source/boost_${BOOST_VERSION}.tar.bz2"
 
 if [ "$NO_CLEANUP" = "1" ] ; then
     TMPD=$HOME/TMP/boost
@@ -23,19 +25,19 @@ cleanup()
 }
 
 cd $TMPD
-FILE=boost_${BOOST_VERSION}.tar.gz
-! [ -f "$FILE" ] && wget https://dl.bintray.com/boostorg/release/$(echo $BOOST_VERSION | sed 's,_,.,g')/source/boost_${BOOST_VERSION}.tar.gz
+FILE=boost_${BOOST_VERSION}.tar.bz2
+! [ -f "$FILE" ] && wget "$URL"
 
 [ -d "boost_${BOOST_VERSION}" ] && rm -rf boost_${BOOST_VERSION}
 
-tar -xf boost_${BOOST_VERSION}.tar.gz
+tar -xf boost_${BOOST_VERSION}.tar.bz2
 cd boost_${BOOST_VERSION}
 cp tools/build/example/user-config.jam $HOME
 
-echo "using python : 3.6 : /usr/bin/python3 : /usr/include/python3.6m : /usr/lib ;" >> $HOME/user-config.jam
+#echo "using python : 3.6 : /usr/bin/python3 : /usr/include/python3.6m : /usr/lib ;" >> $HOME/user-config.jam
 
 #  --with-libraries=filesystem,system,python,graph
 ./bootstrap.sh --prefix=$PREFIX
-./b2 -j $(expr $(nproc) \* 3 / 2) toolset=clang cxxstd=17
-sudo ./b2 install toolset=clang cxxstd=17
+./b2 -j $(expr $(nproc) \* 3 / 2) toolset=gcc cxxstd=2a --with-system 
+sudo ./b2 install toolset=gcc cxxstd=2a
 
