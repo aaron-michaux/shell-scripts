342416ba2701e88397a301f8b8ebb87bdae5223f
Aaron Michaux <aaron@pageofswords.net>
2022-12-09 06:06:26 -0500

    notes on how to create a sink, and record its playback

---
+++ bin/z-record.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cat <<EOF
+
+# 1. Create the sink
+SINK_NAME="MySink"
+pacmd load-module module-null-sink sink_name=$SINK_NAME
+pacmd update-sink-proplist MySink device.description=$SINK_NAME
+
+# 2. List links to ensure it exists
+pacmd list-sinks | grep -e 'name:' -e 'index' -e 'Speakers'
+
+# 3. Use "Pulse Audio Volume Control" to route output to the sink
+
+# 4. Start recording!
+parec -d $SINK_NAME.monitor | lame  -r -V0 - out.mp3
+
+
+
+EOF
